9ac14086435e11360630ed630a5dffdfe7c42058
Hiroshi Ueki <hirocueki@gmail.com>
2018-01-31 17:30:21 +0900

    Add build_quest method

--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,65 @@
 <template>
   <section class="container">
     <div>
-      <app-logo/>
-      <h1 class="title">
-        joycalc
-      </h1>
-      <h2 class="subtitle">
-        Enjoy calculate
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
+      <div class="center">
+        <h1 id='result-box'>{{result}}</h1>
+        <h1 class='quest'>{{quest}}</h1>
+        <input class='answer' type="number" v-model='ur_answer' id="answer">
+      </div>
+      <div>
+        <button class='button--green' @click='calc'>こたえる！</button>
+      </div>
+      <br>
+      <div>
+        <button class='button--green' @click='build_quest'>つぎの問題</button>
       </div>
     </div>
   </section>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
-
 export default {
-  components: {
-    AppLogo
+  data () {
+    return {
+      quest: '',
+      answer: 0,
+      ur_answer: '',
+      result: '',
+      difficult: 0
+    }
+  },
+  computed: {
+    
+  },
+  created: function () {
+    this.build_quest()
+  },
+  methods: {
+
+    build_quest: function () {
+      var l = Math.floor( Math.random() * 100 );
+      var r = Math.floor( Math.random() * 100 );
+      this.quest = l+'+'+r
+      this.answer = l+r
+    },
+    focues_in: function (selector) {
+      document.querySelector(selector).focus()
+    },
+    is_collect: function () {
+      console.log(this.ur_answer )
+      console.log( this.answer)
+      return this.ur_answer == this.answer
+    },
+    calc () {
+      if (this.is_collect()){
+        this.result = '正解！'
+        this.build_quest()
+        this.focues_in('#answer')
+        return
+      } 
+      this.result = 'ちがうよ！'
+      this.focues_in('#answer')
+    }
   }
 }
 </script>
@@ -41,7 +73,7 @@ export default {
   text-align: center;
 }
 
-.title {
+.quest {
   font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
   display: block;
   font-weight: 300;
@@ -50,12 +82,13 @@ export default {
   letter-spacing: 1px;
 }
 
-.subtitle {
+.answer {
   font-weight: 300;
   font-size: 42px;
   color: #526488;
   word-spacing: 5px;
   padding-bottom: 15px;
+  text-align: center;
 }
 
 .links {
