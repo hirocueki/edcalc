8d4e4a4a763f96b47a2d6cd66ffe3a8df960a43a
Hiroshi Ueki <hirocueki@gmail.com>
2018-02-01 22:13:25 +0900

    Add numpad buttons

--- pages/index.vue
+++ pages/index.vue
@@ -4,15 +4,33 @@
       <div class="center">
         <div id='result-box'>{{result}}</div>
         <div class='quest'>{{quest}}</div>
-        <input class='answer' type="number" v-model.number='ur_answer'>
+        <input class='answer' type="number" readonly v-model.number='ur_answer'>
+        
+        
+      </div>
+      <div class="numpad">
+        <br>
+        <button class='button--green' @click='contains(7)'>7</button>
+        <button class='button--green' @click='contains(8)'>8</button>
+        <button class='button--green' @click='contains(9)'>9</button>
+        <br>
+        <button class='button--green' @click='contains(4)'>4</button>
+        <button class='button--green' @click='contains(5)'>5</button>
+        <button class='button--green' @click='contains(6)'>6</button>
+        <br>
+        <button class='button--green' @click='contains(1)'>1</button>
+        <button class='button--green' @click='contains(2)'>2</button>
+        <button class='button--green' @click='contains(3)'>3</button>
+        <br>
+        <button class='button--green' @click='contains(0)'>0</button>
+        <button class='button--green' @click='clear'>AC</button>
+        <button class='button--green' @click='revert'>-/+</button>
+        
       </div>
       <div>
-        <button class='button--green' @click='calc'>こたえる！</button>
+        <button class='button-answer button--green' @click='calc'>こたえる！</button>
       </div>
       <br>
-      <!-- <div>
-        <button class='button--green' @click='build_quest'>つぎの問題</button>
-      </div> -->
     </div>
   </section>
 </template>
@@ -23,7 +41,7 @@ export default {
     return {
       quest: '',
       answer: 0,
-      ur_answer: '',
+      ur_answer: 0,
       result: '',
       difficult: 0
     }
@@ -35,7 +53,15 @@ export default {
     this.build_quest()
   },
   methods: {
-
+    revert: function () {
+      this.ur_answer = this.ur_answer*-1+'' 
+    },
+    clear: function () {
+      this.ur_answer = ''
+    },
+    contains: function (s) {
+      this.ur_answer += s 
+    },
     build_quest: function () {
       var l = Math.floor( Math.random() * 100 );
       var r = Math.floor( Math.random() * 100 );
@@ -80,7 +106,14 @@ export default {
   align-items: center;
   text-align: center;
 }
-
+.button-answer{
+  width:15rem;
+  height: 3rem;
+}
+.numpad > button{
+  width:5rem;
+  height: 4rem;
+}
 .quest {
   font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
   display: block;
